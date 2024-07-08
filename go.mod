bc27fa4dd353f7b5fe5fad0ed3d35c8917639415
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-07-08 17:02:00 +0530

    fix: common env file for front and backend

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- main.go
+++ main.go
@@ -2,29 +2,34 @@ package main
 
 import (
 	"context"
-	"yacc/backend"
 	"log"
 	"os"
+	"yacc/backend"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
-var Pool *pgxpool.Pool
+func init() {
+	env := os.Getenv("GO_ENV")
+	if env == "" {
+		env = "production"
+	}
 
-func main() {
-	err := godotenv.Load()
+	log.Printf("running in %s mode\n", env)
+	err := godotenv.Load(".env." + env + ".local")
 	if err != nil {
 		log.Fatal("Error loading .env")
 	}
+}
+
+func main() {
 
 	pool, err := pgxpool.New(context.Background(), os.Getenv("POSTGRES_URL"))
 	if err != nil {
 		log.Fatal("Unable to connect to db")
 		os.Exit(1)
 	}
-	Pool = pool
-	
 
 	defer pool.Close()
 
