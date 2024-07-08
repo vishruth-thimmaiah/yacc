0e8a7b0057cc3075ce62c6cce9ddaa8e09556986
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-07-08 17:02:45 +0530

    feat: logout

--- frontend/src/views/settings.vue
+++ frontend/src/views/settings.vue
@@ -14,14 +14,20 @@
 				<option value="dark">Dark</option>
 			</select>
 		</div>
+
+		<h3>Account</h3>
+		<hr />
+		<div class="logout">
+			<label for="logout">Logout</label>
+			<button @click="Logout"> Logout</button>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+import axios from 'axios';
 import { ref } from 'vue';
-
-
-
+import { useRouter } from 'vue-router';
 
 const darkMode = ref<string>(localStorage.getItem("user-theme") || "system")
 function changeMode() {
@@ -33,6 +39,13 @@ function changeMode() {
 	} else {
 		document.documentElement.className = theme;
 	}
+}
+
+const router = useRouter()
+async function Logout() {
+	await axios.get("/api/auth/logout").then(function () {
+		router.push("/login")
+	})
 }
 
 </script>
@@ -92,6 +105,28 @@ function changeMode() {
 			padding: 7px 10px;
 			border-radius: 5px;
 			background: var(--secondary-color);
+			font-size: 15px;
+			width: 40%;
+		}
+	}
+
+	.logout {
+		color: var(--text-primary-color);
+		font-family: "M PLUS Rounded 1c", sans-serif;
+		font-size: 15px;
+
+		label {
+			user-select: none;
+			display: inline-block;
+			width: 60%;
+		}
+
+		button {
+			color: var(--text-primary-color);
+			border: none;
+			padding: 7px 10px;
+			border-radius: 5px;
+			background-color: #e6665c;
 			font-size: 15px;
 			width: 40%;
 		}
